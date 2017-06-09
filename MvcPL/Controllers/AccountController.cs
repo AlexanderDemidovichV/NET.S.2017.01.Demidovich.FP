cffd20550679a34c14810ad9a0c53bc41cefc2ca
PC <PC@DESKTOP-QVDD3Q5>
2017-06-09 08:43:58 +0300

    making registration

---
+++ MvcPL/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPL.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
